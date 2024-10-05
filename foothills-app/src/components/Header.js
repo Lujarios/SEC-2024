3128c77db01735ce8c5e165a6c0f604aae580f7f
Evan Mann <ejmann900@gmail.com>
2024-10-05 16:48:27 -0600

    added id checking for header

--- foothills-app/src/components/Header.js
+++ foothills-app/src/components/Header.js
@@ -2,21 +2,42 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Header.css"
 
+
+
 function Header() {
-    const [ID, setID] = useState(-1);
+
+    const [patientIds, setPatientIds] = useState([]);
+    // Fetch all patient IDs
+    const fetchPatientIds = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/api/patientIds');
+            const patientIdsData = await response.json();
+            setPatientIds(patientIdsData);
+        } catch (error) {
+            console.error('Error fetching patient IDs:', error);
+        }
+    };
 
     const navigate = useNavigate();
-    const handleButtonClick = (route) => {
-        setID(document.getElementById("id-input-field").value);
-        console.log(ID);
-        navigate(route, { state: { key: ID } });
+    const handleButtonClick = () => {
+        var inputID = document.getElementById("id-input-field").value;
+        fetchPatientIds();
+        for (const id in patientIds) {
+            if (inputID == id) {
+                navigate("/Patient", { state: { key: id } });
+                return
+            }
+        }
+        navigate("/Error");
+
+
     }
     return (
 
         <div className='Header'>
             <h2 className='Title'>Foothills Hospital</h2>
             <input id="id-input-field" className="id_input" placeholder='Input Patient ID'></input>
-            <button onClick={() => handleButtonClick("/Patient")}>Search</button>
+            <button onClick={() => handleButtonClick()}>Search</button>
         </div>
     );
 };
